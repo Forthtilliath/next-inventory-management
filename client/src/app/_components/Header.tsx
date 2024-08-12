099902e0d959b5ab9d22505e577f03d4ff33b986
Forthtilliath <forth@live.fr>
2024-08-12 21:52:45 +0200

    chore: convert to function

--- client/src/app/_components/Header.tsx
+++ client/src/app/_components/Header.tsx
@@ -1,5 +1,3 @@
-const Header = ({ children }: WithChildren) => {
+export function Header({ children }: WithChildren) {
 	return <h1 className="text-2xl font-semibold text-gray-700">{children}</h1>;
-};
-
-export default Header;
+}
