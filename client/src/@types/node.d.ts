2c1fd1e89b750d8f8b87f51e984fc6b75e88ab65
Forthtilliath <forth@live.fr>
2024-08-17 16:22:34 +0200

    chore: change NEXT_PUBLIC_API_PORT type

--- client/src/@types/node.d.ts
+++ client/src/@types/node.d.ts
@@ -3,7 +3,7 @@ declare global {
     interface ProcessEnv {
       // app
       NEXT_PUBLIC_API_HOST: string;
-      NEXT_PUBLIC_API_PORT: string;
+      NEXT_PUBLIC_API_PORT: number;
       NEXT_PUBLIC_API_BASE_URL: string;
     }
   }
