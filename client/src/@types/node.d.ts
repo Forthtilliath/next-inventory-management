3393326a7ffc23352ac459a92741b268360cf4ac
Forthtilliath <forth@live.fr>
2024-08-17 16:22:06 +0200

    build: clean up packages

--- client/src/@types/node.d.ts
+++ client/src/@types/node.d.ts
@@ -1,10 +1,12 @@
 declare global {
-	namespace NodeJS {
-		interface ProcessEnv {
-			// app
-			NEXT_PUBLIC_API_BASE_URL: string;
-		}
-	}
+  namespace NodeJS {
+    interface ProcessEnv {
+      // app
+      NEXT_PUBLIC_API_HOST: string;
+      NEXT_PUBLIC_API_PORT: string;
+      NEXT_PUBLIC_API_BASE_URL: string;
+    }
+  }
 }
 
 export type {};
